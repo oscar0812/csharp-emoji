c3ac61bc67b79e73ee35bf81f709186f51bed038
oscar0812 <oscar0812torres@gmail.com>
2017-11-23 22:07:49 -0600

    Example use

--- Emoji/Program.cs
+++ Emoji/Program.cs
@@ -8,64 +8,12 @@ namespace Emoji
 {
     class Program
     {
-        private List<Emoji> EmojiList { get; set; }
-        private EmojiQuery EmojiQuery { get; set; }
-
-        public Program()
-        {
-            Start();
-        }
-
-        // only called when class is instantiated
-        void Start()
-        {
-            // set the fields once, so no extra work is done later
-            EmojiList = JsonConvert.DeserializeObject<List<Emoji>>
-                (File.ReadAllText(@"emojis.json"));
-
-            EmojiQuery = new EmojiQuery(EmojiList);
-
-        }
-
-        public List<Emoji> GetAllEmojis()
-        {
-            return EmojiList;
-        }
-
-        public List<Emoji> SearchByDescription(string desc)
-        {
-            return EmojiQuery.SearchByDescription(desc);
-        }
-
-        public List<Emoji> SearchByCategory(string cat)
-        {
-            return EmojiQuery.SearchByCategory(cat);
-        }
-        
-        public List<Emoji> SearchByAlias(string alias)
-        {
-            return EmojiQuery.SearchByAlias(alias);
-        }
-        
-        public List<Emoji> SearchByTag(string alias)
-        {
-            return EmojiQuery.SearchByTag(alias);
-        }
-
-        public static void PrintList(List<Emoji> list)
-        {
-            foreach (var e in list)
-            {
-                Console.WriteLine(e);
-            }
-
-        }
-
         static void Main(string[] args)
         {
-            var program = new Program();
-            PrintList(program.SearchByCategory("Activity"));
-            PrintList(program.SearchByCategory("Activity"));
+            EmojiList list = new EmojiList();
+            EmojiList.PrintList(list.SearchByDescription("c").SearchByDescription("sm").
+                SearchByDescription("face").SearchByCategory("people"));
+            Console.WriteLine("DONE");
         }
     }
 }
